855cdeb4208d94c1c520d3b6ced194183286cfdf
Christopher Moseley <cs@csmoseley.com>
2016-12-15 06:03:00 +0000

    Tweaking Addition of tasks

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -4,7 +4,7 @@ $.get( "tasks.json", function( data ) {
   container.empty(); for(i=0; i < data.length; i++){
   container.append('<li data-obj-id="'+data[i].id+'" class="list-group-item ' + getStatusColor(data[i].status) + '"><button type="button" class="close" onclick="removeTask(this)"><span>&times;</span></button>'+data[i].name+'</li>');
 }
-container.append("<li class='list-group-item'><input class='form-control' onfocusout='addTask(this, \"Dining Room\")' placeholder='Add a new task'></li>");
+container.append("<li class='list-group-item'><div class='input-group'><input class='form-control' placeholder='Add a New Task'><span class='input-group-btn'><button type='button' class='btn btn-success' onclick='addTask(this, \"Dining Room\")'>+</button></span></div></li>");
 });
 }
 
@@ -22,10 +22,11 @@ function removeTask(element){
 }
 
 function addTask(element, room){
+taskName = element.parentElement.previousSibling.value;
 $(element).addClass("disabled");
-console.log(element.value + ":" + room);
-if(element.value.trim != ""){ $.post("tasks.json", {task: {room: room, status: 0, name: element.value}});
-refresh();
+console.log(taskName + ":" + room);
+if(taskName.trim != ""){ 
+$.post("tasks.json", {task: {room: room, status: 0, name: taskName}}, function(result){refresh();});
 }
 }
 
